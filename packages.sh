3400ae8cf8809f590c0de3e8275d7d75f53a3e26
Thomas Rasshofer <hello@thomasrasshofer.com>
2016-11-03 14:14:03 +0100

    Import latest configuration and packages

--- packages.sh
+++ packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apm install Sublime-Style-Column-Selection
 apm install atom-beautify
+apm install atom-jade
 apm install copy-path
 apm install editorconfig
 apm install emmet
@@ -9,6 +10,7 @@ apm install highlight-selected
 apm install language-babel
 apm install language-docker
 apm install language-latex
+apm install language-pug
 apm install minimap
 apm install monokai
 apm install multi-cursor
